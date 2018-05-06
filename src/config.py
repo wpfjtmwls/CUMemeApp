53df9741596f343c5e06fbad10da72c19c03a9e1
Alex Yoo <ay244@cornell.edu>
2018-05-05 20:52:57 -0400

    configurations done

--- src/config.py
+++ src/config.py
@@ -21,10 +21,7 @@ class Config(object):
   DB_PASS = os.environ['DB_PASS']
   DB_SERVICE = os.environ['DB_SERVICE']
   DB_PORT = os.environ['DB_PORT']
-  SQLALCHEMY_DATABASE_URI = 'postgresql://{0}:{1}@{2}:{3}/{4}'.format(
-      DB_USER, DB_PASS, DB_SERVICE, DB_PORT, DB_NAME
-  )
-
+  SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 class ProductionConfig(Config):
   """
   Production environment app configuration
