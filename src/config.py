8f3cb660096be94b13ce5b38dfe066a2207a47b3
Alex Yoo <ay244@cornell.edu>
2018-05-05 20:55:49 -0400

    config redone

--- src/config.py
+++ src/config.py
@@ -16,11 +16,7 @@ class Config(object):
   THREADS_PER_PAGE = 2
   
   # Database info
-  DB_NAME = os.environ['DB_NAME']
-  DB_USER = os.environ['DB_USER']
-  DB_PASS = os.environ['DB_PASS']
-  DB_SERVICE = os.environ['DB_SERVICE']
-  DB_PORT = os.environ['DB_PORT']
+
   SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 class ProductionConfig(Config):
   """
